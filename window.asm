035142595e5bb99d66f26b991ff6778d26137001
Micha <67926505+MichaHere@users.noreply.github.com>
2025-07-22 15:10:02 +0200

    Implemented connecting to the X11 server

--- window.asm
+++ window.asm
@@ -1,13 +1,20 @@
 BITS 64                                         ; use 64 bits
 CPU X64                                         ; target x86_64 CPU family
 
+section .rodata
+
+sun_path: db "/tmp/.X11-unix/X1", 0
+static sun_path:data
+
+
 section .text
 
-%define AF_UNIX         1
-%define SOCK_STREAM     1
+%define AF_UNIX             1
+%define SOCK_STREAM         1
 
-%define SYSCALL_SOCKET  41
-%define SYSCALL_EXIT    60
+%define SYSCALL_SOCKET      41
+%define SYSCALL_CONNECT     42
+%define SYSCALL_EXIT        60
 
 ; Create a UNIX socket and connect to the X11 server
 ; @returns Socket file descriptor
@@ -29,8 +36,36 @@ static connect_to_x11_server:function
 
     mov         rdi, rax                        ; store socket file descriptor in the rdi register
 
+    sub         rsp, 112                        ; reserve space to store the sockaddr_un on the stack
+
+    mov         WORD [rsp], AF_UNIX             ; set sockaddr_un.sun_family to AF_UNIX
+
+    ; fill sockaddr_un.sun_path with "/tmp/.X11-unix/X0"
+    lea         rsi, sun_path                   ; load the address of the string to rsi
+    mov         r12, rdi                        ; save the socket file descriptor from rdi in r12
+    lea         rdi, [rsp + 2]
+    cld                                         ; more forward
+    mov         ecx, 19                         ; length of string is 19 with null terminator
+    rep         movsb                           ; copy
+
+    ; connect to the server
+    mov         rax, SYSCALL_CONNECT
+    mov         rdi, r12                        ; restore the file descriptor to rdi
+    lea         rsi, [rsp]
+
+    %define SIZEOF_SOCKADDR_UN 2+108
+
+    mov         rdx, SIZEOF_SOCKADDR_UN
+    syscall
+
+    cmp         rax, 0
+    jne         exit_on_error
+
+    mov         rax, rdi                        ; return the socket file descriptor
+
     ; function epilogue
-    pop rbp                                     ; retore base pointer
+    add         rsp, 112                        ; restore the stack
+    pop         rbp                             ; restore base pointer
     ret
 
 exit_on_error:
@@ -43,6 +78,8 @@ exit_on_error:
 
 _start:
 global _start:function
+    call connect_to_x11_server
+
     ; exit program
     mov         rax, SYSCALL_EXIT
     mov         rdi, 0                          ; error code
